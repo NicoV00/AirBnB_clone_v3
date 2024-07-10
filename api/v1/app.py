8243ab3c2673477fca787dfb3d2e014027fc46e1
NicoV00 <6328@holbertonstudents.com>
2024-07-10 11:14:50 -0700

    changed pep, now corrected

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def app_close(exception=None):
     """method close storage"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000', threaded=True)
